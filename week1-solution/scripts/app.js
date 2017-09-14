dbf9bb28e3e7b595a55610e4ef50bbb2111b98cd
Azeez <azeez.otori@gmail.com>
2017-09-14 13:36:14 -0700

    finish week 1 solution

--- week1-solution/scripts/app.js
+++ week1-solution/scripts/app.js
@@ -8,28 +8,43 @@
 	function LunchCheckController($scope) {
 		var comma = ',';
 
+		$scope.isSuccess = false;
+
+		$scope.isFailure = false;
 
 		$scope.foodNames = "";
 
 		$scope.message = "";
 
 		$scope.checkLunch = function() {
-			var foodNames = $scope.foodNames.replace(/[, ]+/g, ',').trim();
+			// remove empty items; i.e. , ,
+			var foodNames = $scope.foodNames.replace(/,[, ]+/g, ',').trim();
+
+			// remove leading comma, if any
 			foodNames = foodNames.replace(/^,/, '');
+
+			// remove trailing comma, if any
 			foodNames = foodNames.replace(/,$/, '');
+
 			$scope.foodNames = foodNames;
 			var numFoods = (foodNames.length === 0) ? 0 : foodNames.split(comma).length;
 
 			if (numFoods === 0) 
 			{
 				$scope.message = "Please enter data first";
+				$scope.isSuccess = false;
+				$scope.isFailure = false;
 			}
 			else if(numFoods <= 3)
 			{
 				$scope.message = "Enjoy!";
+				$scope.isSuccess = true;
+				$scope.isFailure = false;
 			}
-			else {
+			else if(numFoods > 3) {
 				$scope.message = "Too Much!";
+				$scope.isSuccess = false;
+				$scope.isFailure = true;
 			}
 		};
 
