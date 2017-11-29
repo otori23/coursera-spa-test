740541fd01d725c2ac5ce3737ca3cfb4a268cea8
Azeez <azeez.otori@gmail.com>
2017-11-28 23:24:23 -0800

    add unit test for favorite directive

---
+++ week5-solution/specs/favorite.directive.spec.js
@@ -0,0 +1,57 @@
+describe('The favorite directive', function() {
+    'use strict';
+
+    var $httpBackend;
+    var ApiPath;
+    var $compile;
+    var $rootScope;
+    var html;
+
+    beforeEach(function() {
+        // Load module
+        module('common');
+
+        // Inject dependencies
+        inject(function ($injector, _$compile_, _$rootScope_) {
+            $httpBackend = $injector.get('$httpBackend');
+            ApiPath = $injector.get('ApiPath');
+            $compile = _$compile_;
+            $rootScope = _$rootScope_;
+        });
+
+        html = 
+        '<form name="form">' +
+        '<input type="text" name="favorite" ng-model="signupCtrl.favorite" favorite>' +
+        '</form>';
+    });
+  
+    it('should find an exisiting favorite', function() {
+        $httpBackend.whenGET(ApiPath + '/menu_items/A10.json').respond(200, {message: 'success'});
+
+        $rootScope.signupCtrl = {favorite: null};
+
+        var element = $compile(html)($rootScope);
+        
+        $rootScope.form.favorite.$setViewValue('A10');
+        $rootScope.$digest();
+
+        $httpBackend.flush();
+
+        expect($rootScope.form.favorite.$valid).toBe(true);
+    });
+
+    it('should not find an undefined favorite', function() {
+        $httpBackend.whenGET(ApiPath + '/menu_items/A100.json').respond(400, {message: 'error'});
+
+        $rootScope.signupCtrl = {favorite: null};
+
+        var element = $compile(html)($rootScope);
+        
+        $rootScope.form.favorite.$setViewValue('A100');
+        $rootScope.$digest();
+
+        $httpBackend.flush();
+
+        expect($rootScope.form.favorite.$invalid).toBe(true);
+    });
+  });
